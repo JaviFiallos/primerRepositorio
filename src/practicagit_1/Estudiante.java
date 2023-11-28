b1ad38d3ab18b9e6a83a6f685834502a76a582cc
JaviF <fiallosedisonreggae@gmail.com>
2023-11-27 21:10:58 -0500

    Commit inicial

---
+++ src/practicagit_1/Estudiante.java
@@ -0,0 +1,5 @@
+package practicagit_1;
+
+public class Estudiante {
+    private String cambio;
+}
---
+++ src/practicagit_1/PracticaGIT_1.java
@@ -0,0 +1,17 @@
+
+package practicagit_1;
+
+/**
+ *
+ * @author PC
+ */
+public class PracticaGIT_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
